b6aa5f4963ff6c800b0dd33fa249a40ace4205e5
apcflencomienda <flencomienda@student.apc.edu.ph>
2025-09-04 09:05:37 +0800

    commit 8

---
+++ js/vue.js
@@ -0,0 +1,11 @@
+  const app = Vue.createApp({
+   data() {
+    return {
+     message: "Hello WEBPROG AFD241 World! With separate CSS and JS files",
+     vueClass: "pinkBG"
+
+    }
+   }
+  })
+  
+  app.mount('#app')
